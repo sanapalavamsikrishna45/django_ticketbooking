4297b264610c4ce9a79ce71abb8f07570e97bfc2
sanapalavamsikrishna45 <vamsisanapal2@gmail.com>
2025-08-12 18:43:01 +0530

    edits

--- svm_travels/BusRoute/models.py
+++ svm_travels/BusRoute/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
+from mainapp.models import Stop
 # Create your models here.
 
 
 
-# class BusRoute(models.Model):
-#     start = models.ForeignKey()
-#     end = models.ForeignKey
-#     dist = models.IntegerField()
-# 0
+class BusRoute(models.Model):
+    start = models.ForeignKey(Stop, on_delete = models.CASCADE, related_name = 'route_start')
+    end = models.ForeignKey(Stop, on_delete=models.CASCADE, related_name = 'route_end')
+    dist = models.IntegerField()
 
-
-#     def __str__(self):
-#         return
+    def __str__(self):
+        return f"BusRoute From {self.start} To {self.end}"
